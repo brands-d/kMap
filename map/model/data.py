bc9c6a41263db10b7c3bb11c7e15f2a323ddd251
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-07-07 23:23:39 +0200

    New SlicedData class

--- map/model/data.py
+++ map/model/data.py
@@ -16,19 +16,14 @@ class PlotData():
     Args:
         data (float): 2D array of numbers with at least 2 elements in
             each dimension. Can contain NaN values.
-        x_axis (float): 1D array of numbers. Cannot contain NaN values.
-            Has to have the same length as the first dimension of data.
-        y_axis (float): 1D array of numbers. Cannot contain NaN values.
-            Has to have the same length as the second dimension of data.
-
-    Attributes:
-        data (float): See Args.
-        x_axis (float): See Args.
-        y_axis (float): See Args.
         range(float): List of two lists containing the min and max value
             of x_axis and y_axis. (E.g. [[x_min, x_max],[y_min, y_max]])
-        step_size(float): Contains the 
 
+    Attributes:
+        data (float): See args.
+        range (float): See args.
+        x_axis (float): 1D array for the first axis of data.
+        y_axis (float): 1D array for the first axis of data.
     """
 
     def __init__(self, data, range_):
@@ -46,3 +41,42 @@ class PlotData():
                                   dtype=np.float64)
         self.step_size = [self.x_axis[1] - self.x_axis[0],
                           self.y_axis[1] - self.y_axis[0]]
+
+
+class SlicedData():
+
+    def __init__(self, slices, ranges, slice_axis):
+
+        # Test for uniqueness in the slice_axis key list
+        if len(slice_axis) > len(set(slice_axis)):
+            raise ValueError('slice_axis has to have only unique values')
+
+        # Test if slice_axis matches slices
+        elif len(slice_axis) != len(slices):
+            raise TypeError('slice_axis has to be the same length as slices')
+
+        else:
+            self.slice_axis = slice_axis
+
+        if len(np.array(ranges).shape) == 2:
+            self._slices = [PlotData(slice_, ranges) for slice_ in slices]
+
+        elif len(ranges) == len(self._slices):
+            self._slices = [PlotData(slice_, range_)
+                            for slice_, range_ in zip(slices, ranges)]
+        else:
+            raise TypeError(
+                'range_ needs to be specified either once for all or for all \
+                slices individually')
+
+    def slice_from_idx(self, idx):
+
+        return self._slices[idx]
+
+    def slice_from_value(self, value):
+
+        for idx, key in enumerate(self.slice_axis):
+            if key == value:
+                return self._slices[idx]
+
+        return None
--- map/tests/test.py
+++ map/tests/test.py
@@ -1,6 +1,7 @@
 import unittest
+import numpy as np
 import numpy.testing
-from map.model.data import PlotData
+from map.model.data import PlotData, SlicedData
 
 
 class TestPlotData(unittest.TestCase):
@@ -9,14 +10,29 @@ class TestPlotData(unittest.TestCase):
         data = [[1, 2, 3], [4, 5, 6]]
         range_ = [[1, 2], [-1, 1]]
 
-        plotdata = PlotData(data, range_)
+        plot_data = PlotData(data, range_)
 
-        numpy.testing.assert_equal(plotdata.data, data)
-        numpy.testing.assert_equal(plotdata.range, range_)
-        numpy.testing.assert_equal(plotdata.shape, (2, 3))
-        numpy.testing.assert_equal(plotdata.x_axis, [1, 2])
-        numpy.testing.assert_equal(plotdata.y_axis, [-1, 0, 1])
-        numpy.testing.assert_equal(plotdata.step_size, [1, 1])
+        numpy.testing.assert_equal(plot_data.data, data)
+        numpy.testing.assert_equal(plot_data.range, range_)
+        numpy.testing.assert_equal(plot_data.shape, (2, 3))
+        numpy.testing.assert_equal(plot_data.x_axis, [1, 2])
+        numpy.testing.assert_equal(plot_data.y_axis, [-1, 0, 1])
+        numpy.testing.assert_equal(plot_data.step_size, [1, 1])
+
+
+class TestSlicedData(unittest.TestCase):
+    def test_initialization(self):
+
+        slices = np.arange(27).reshape(3, 3, 3)
+        range_ = [[1, 2], [-1, 1]]
+        slice_axis = [1, 2, 3]
+
+        sliced_data = SlicedData(slices, range_, slice_axis)
+
+        numpy.testing.assert_equal(
+            sliced_data.slice_from_idx(1).data, slices[1])
+        numpy.testing.assert_equal(
+            sliced_data.slice_from_value(1).data, slices[0])
 
 
 if __name__ == '__main__':
