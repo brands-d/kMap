918b501ba72546ef53824dd650e71f8a684727f1
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-07-18 12:01:20 +0200

    Figured out GUI Testing

---
+++ map/tests/test_tabwidget.py
@@ -0,0 +1,26 @@
+import unittest
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtTest import QTest as qt
+from map.model.model import Model
+from map import Map
+from map.view.mainwindow import MainWindow
+
+
+class TestMainWindow(unittest.TestCase):
+
+    app = Map.Map([])
+
+    def setUp(self):
+
+        self.model = Model()
+        self.window = MainWindow(self.model)
+
+    def test_default_state(self):
+
+        self.assertEqual(self.window.tab_widget.count(), 0)
+
+    '''UNDER CONSTRUCTION
+    def test_default_state(self):
+
+        qt.keySequence(self.window, QKeySequence('Ctrl+f'))'''
