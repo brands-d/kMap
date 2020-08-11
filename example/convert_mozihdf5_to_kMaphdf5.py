77c20bdcc51f94b445ad4dbc5c156d1c0347f31a
pep <peter.puschnig@uni-graz.at>
2020-08-11 08:37:12 +0200

    conversion script convert_mozihdf5_to_kMaphdf5.py for hdf5-files

---
+++ example/convert_mozihdf5_to_kMaphdf5.py
@@ -0,0 +1,75 @@
+import h5py
+import numpy as np
+
+# this script demonstrates how the hdf5-files created by Mozi can be converted to 
+# the format used in kMap.py 
+
+
+def mozi_to_kmap(mozi_h5_file,kmap_h5_file,
+                 name=None,           # if None than 'filenumber' from old hdf5 is taken, otherwise name must be a string
+                 axis1type='+BE'):    # choose between '+BE' or '-BE' (binding energy) or 'E_kin' (kinetic energy)
+
+    old     = h5py.File(mozi_h5_file,'r')  # open old hdf5 file
+    new     = h5py.File(kmap_h5_file,'w')  # create new file
+
+    # extract information from old hdf5
+    oldkeys = ['filenumber','fermiLevel','kStepSize','list_index','list_kAxis','list_BE_real','list_BE',
+                'arcwidth','polarshift','rotation','negPolar_avgs','sym_anglemin','sym_anglemax','symmode']
+    filenumber   = old['filenumber'][()]
+    fermiLevel   = old['fermiLevel'][()]
+    list_kAxis   = old['list_kAxis'][()]
+    list_BE_real = old['list_BE_real'][()]
+    list_BE      = old['list_BE'][()]
+
+    if name == None: name = filenumber
+
+    if   axis1type == '+BE':   # binding energy with positive sign convention
+        axis_1_range = [ list_BE_real[-1], list_BE_real[0]]      
+    
+    elif axis1type == '-BE':   # binding energy with negative sign convention  
+        axis_1_range = [-list_BE_real[0], -list_BE_real[-1]] 
+
+    elif axis1type == 'E_kin':   # kinetic energy 
+        axis_1_range = [fermiLevel-list_BE_real[0], fermiLevel-list_BE_real[-1]] 
+
+    
+    # write header info in new hdf5
+    new.create_dataset('name',        data=name)
+    new.create_dataset('axis_1_label',data=axis1type)
+    new.create_dataset('axis_2_label',data='kx')
+    new.create_dataset('axis_3_label',data='ky')
+    new.create_dataset('axis_1_units',data='eV')
+    new.create_dataset('axis_2_units',data='1/A')
+    new.create_dataset('axis_3_units',data='1/A')
+    new.create_dataset('axis_1_range',data=axis_1_range)
+    new.create_dataset('axis_2_range',data=[list_kAxis[0,0],list_kAxis[0,-1]])
+    new.create_dataset('axis_3_range',data=[list_kAxis[0,0],list_kAxis[0,-1]])
+
+    # extract data from old hdf5 and write 3D-array to new hdf5
+    nx    = len(list_BE)
+    ny    = list_kAxis.shape[1]
+    nz    = ny
+    data  = np.zeros((nx,ny,nz))
+   
+    for count, BE in enumerate(old['data_kmaps']):
+        if axis1type == '+BE':
+            i = count
+        else:
+            i = nx - count - 1
+
+        kxkyslice = old['data_kmaps'][BE][()]
+        data[i,:,:] = kxkyslice
+
+    new.create_dataset('data',data=data,
+                        dtype='f8',compression='gzip', compression_opts=9)
+
+    old.close()
+    new.close()
+    return
+
+
+
+mozi_to_kmap('kmaps_6584_BEstep0.02_kStep0.02.hdf5','6584.hdf5',axis1type='E_kin')
+
+
+
--- example/demo_sliced_data.py
+++ example/demo_sliced_data.py
@@ -1,24 +1,23 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
 # Import the sliced data class from the model folder
-from kmap.model.sliceddata import SlicedData
+from kmap.library.sliceddata import SlicedData
 import matplotlib.pyplot as plt
 import os
 
 # Get path to the directory this file is in
 dir_path, __ = os.path.split(os.path.realpath(__file__))
-file_path = dir_path + '/basic.hdf5'
+file_path = dir_path + '/6584.hdf5'
 # Loading from file (Initialisation methods are usually class members)
 sliced_data = SlicedData.init_from_hdf5(file_path)
+print(dir(sliced_data))
 
-# Print stored meta_data
-print(sliced_data.meta_data)
-print(sliced_data.slice_keys)
+# Get a PlotData object consisting of the 10th slice and its axes
+plot_data = sliced_data.slice_from_index(10)
 
-# Get a PlotData object consisting of the 0th slice and it's axes
-plot_data = sliced_data.slice_from_idx(0)
-
-# Alternatively get the slice with a slice key (certain energy or time
-# for example)
-plot_data = sliced_data.slice_from_key(2)
 
 # Plot
 __, axes = plt.subplots()
