bda02af4513dfa2a0fe5271d5b24c4cf8a264868
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-10-06 15:23:12 +0200

    Updated version number. Addresses issue 17.
    Version numbers will be used again but from now on only for commits pushed
    to dev or master. Version numbering will follow the following format:
    
    The version number will always contain three numbers separated by a dot.
    The first number denotes the major version, only reserved for big new
    features sets or reworks.
    The second number denotes minor feature updates. Updates here will contain
    at least one new feature and possible bug fixes but the overall usage
    of kMap.py will remain the same.
    The third number denotes bug fixes only. Updates here will contain no new
    features.
    
    Additionally a version number can contain any of the following suffixes:
    -a, -b, -rc
    
    -a being alpha versions. They don't have any user feedback worked in yet.
    -b being beta versions. Some user feedback has been considered and the overall
    performance and usage is close to what the release will be.
    -rc denotes release candidate versions. User feedback has been brought in
    and considered. Some more testing might occur but -rc versions are basically
    ready for release.

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 __project__ = 'kMap.py'
-__version__ = '1.0.0'
-__date__ = '20.09.2020'
+__version__ = '1.1.0-b'
+__date__ = '06.10.2020'
 __directory__ = os.path.dirname(os.path.realpath(__file__))
