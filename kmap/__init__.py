33e7cc6f772a4213bf962cd9ae85f1e1095ee15b
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-10-20 14:34:03 +0200

    v1.3.0-b

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 __project__ = 'kMap.py'
-__version__ = '1.2.0'
-__date__ = '09.10.2020'
+__version__ = '1.3.0-b'
+__date__ = '20.10.2020'
 __directory__ = os.path.dirname(os.path.realpath(__file__))
