66e2a0c2298a27b054fca17f9f4e10ed88e735ac
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-11-15 20:50:00 +0100

    Fix for two bugs for non default lmfit methods (issue 27)
    Not all methods in the lmfit module provide a xtol option. However,
    even two of those that do threw an error (powell, tnc; ampgo has no
    documentation at all). Disabled xtol for all methods except leastsq and least_squares as they are the only ones that didn 't throw one.
    
    All methods except leastsq and least_squares threw errors if a fit with
    not parameter set to be varied was triggered. Caught the error. Nothing
    will now happen but an warning will be printed.

--- kmap/controller/lmfittab.py
+++ kmap/controller/lmfittab.py
@@ -3,6 +3,8 @@
 This file defines two similar types of tabs: the LMFit and the
 LMFitResult tab as well as a common base class LMFitBaseTab.
 """
+# Python Imports
+import logging
 
 # Third Party Imports
 import numpy as np
@@ -151,7 +153,15 @@ class LMFitTab(LMFitBaseTab, LMFitTab_UI):
 
     def trigger_fit(self):
 
-        results = self.model.fit()
+        try:
+            results = self.model.fit()
+
+        except ValueError as e:
+            logging.getLogger('kmap').warning(str(e))
+            self.lmfit_options.update_fit_button()
+            
+            return
+
         settings = self.model.get_settings()
         data = [self.model.sliced_data, self.model.orbitals]
 
