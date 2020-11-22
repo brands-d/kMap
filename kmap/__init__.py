06e3ce57dc78625e4e8d8bf22dddec0e3947f0ba
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-11-22 23:54:37 +0100

    v1.5.0-b

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.4.1-b'
-__date__ = '21.11.2020'
+__version__ = '1.5.0-b'
+__date__ = '22.11.2020'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
