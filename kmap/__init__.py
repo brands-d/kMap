a8a377baaf9369f172d80caba765714f0dee15d6
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2020-12-13 23:58:58 +0100

    Corrected version number

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.6.2'
+__version__ = '1.7.0'
 __date__ = '13.12.2020'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
