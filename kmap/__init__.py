3c4c268f7f4341e6401bea8950e6834958b0fd71
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2021-02-01 00:45:42 +0100

    Bug fix (issue 41)
    Well this awkward... it should be correct now. Hopefully.

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.10.2'
+__version__ = '1.10.3'
 __date__ = '01.02.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
