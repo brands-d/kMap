e4a41f25072d58bcb6a0ce02721c80a905d309e3
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2021-02-17 18:15:06 +0100

    Removed beta status

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.16.0-b'
-__date__ = '15.02.2021'
+__version__ = '1.16.0'
+__date__ = '17.02.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
