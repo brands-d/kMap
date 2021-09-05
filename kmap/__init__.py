3752ddd899cc61d36fc817f977b9a9af8776c90c
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2021-09-05 16:24:41 +0200

    Release of 1.19.0

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.18.0'
-__date__ = '05.04.2021'
+__version__ = '1.19.0'
+__date__ = '05.09.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
