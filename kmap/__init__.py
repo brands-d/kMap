ba1307490448a4800014a15c309394504a6cdc4a
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2021-09-05 16:26:26 +0200

    Set versioning back to beta

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.19.0'
+__version__ = '1.19.0-b'
 __date__ = '05.09.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
