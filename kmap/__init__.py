87b5150bae6d3f75960561d0f5b19b98d95f51bd
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2021-09-08 08:54:51 +0200

    Bump to release version 1.19.2

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '1.19.2-b'
-__date__ = '06.09.2021'
+__version__ = '1.19.2'
+__date__ = '08.09.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
