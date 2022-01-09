4a2a06adbdd8aa74d01ddda7ec07f75936eb8461
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2022-01-09 21:28:20 +0100

    Fixed decimal places bug

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '2.0.0b'
-__date__ = '07.01.2021'
+__version__ = '2.0.1b'
+__date__ = '09.01.2021'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
