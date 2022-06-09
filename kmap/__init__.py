8122f5931ceb3187d9dc6118ab1b6fc9b830147c
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2022-06-09 13:05:27 +0200

    Version bump

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = 'kMap.py'
-__version__ = '2.2.0'
-__date__ = '26.04.2022'
+__version__ = '2.3.0'
+__date__ = '09.06.2022'
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
