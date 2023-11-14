8a2eef79facdf33d406975d6a0d7b8e1ca678bcf
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2023-11-14 17:59:55 +0100

    Updates installation process.

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages
 from kmap import __version__
 
-setup(name='kMap',
-      version=__version__,
-      description='Display, modify and analyse ARPES experiments and DFT \
-      simulations.',
-      packages=find_packages(),
-      license='MIT',
-      url='https://github.com/brands-d/kMap',
-      author='Dominik Brandstetter',
-      author_email='brandstetter.dominik@gmx.net',
-      zip_safe=False,
-      install_requires=['h5py>=3.1.0', 'scipy>=1.5.1',
-                        'matplotlib>=3.3.0', 'numpy>=1.19.0,!=1.19.4',
-                        'PyQt5>=5.15.0', 'PyOpenGL>=3.1.5',
-                        'lmfit>=1.0.1', 'pyqtgraph>=0.11.1',
-                        'qdarkstyle>=2.8.1'],
-      include_package_data=True)
+setup(
+    name="kMap",
+    version=__version__,
+    description="Display, modify and analyse ARPES experiments and DFT \
+      simulations.",
+    packages=find_packages(),
+    license="LGPL",
+    url="https://github.com/brands-d/kMap",
+    author="Dominik Brandstetter",
+    author_email="brandstetter.dominik@uni-graz.at",
+    zip_safe=False,
+    install_requires=[
+        "h5py",
+        "scipy",
+        "matplotlib",
+        "pyside6",
+        "numpy",
+        "pyopengl",
+        "lmfit",
+        "pyqtgraph",
+    ],
+    include_package_data=True,
+)
