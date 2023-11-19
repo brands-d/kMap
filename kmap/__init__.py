99d8f4b8c593c4f078c8e7258772b9b06f9b80f0
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2023-11-19 22:35:22 +0100

    Update to version 3.0.0.

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = "kMap.py"
-__version__ = "3.0.0a3"
+__version__ = "3.0.0"
 __date__ = "19.11.2023"
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
