3389e06a0f7b03b52eecdbe4779a03b4bf4fd1a0
Dominik Brandstetter <dominik.brandstetter@uni-graz.at>
2023-11-20 13:12:22 +0100

    Fixed PyOpenGl demo scripts.

---
+++ example/scripts/demo_PySide_GLVolumeItem2.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+
+import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl  # pip install PyOpenGL
+from PySide6.QtWidgets import QApplication
+
+from kmap.library.orbital import Orbital
+
+### MAIN PROGRAM ######################################################
+
+# Path to data folder; replace with your own; use '/' instead of '+'
+# when concatenating with strings
+data_path = Path(__file__).parent / Path("../data/")
+
+# load data
+cubefile = open(data_path / "bisanthene_HOMO.cube").read()  # read cube-file from file
+orbital = Orbital(cubefile, dk3D=0.15, E_kin_max=150, value="real")
+data = orbital.psi["data"]
+dx, dy, dz = orbital.psi["dx"], orbital.psi["dy"], orbital.psi["dz"]
+nx, ny, nz = data.shape
+x, y, z = orbital.psi["x"], orbital.psi["y"], orbital.psi["z"]
+
+# define color map
+colors = [
+    (255, 0, 0, 100),  # red, semi-transparent
+    (255, 255, 255, 0),  # white, fully transparent
+    (0, 0, 255, 100),  # blue, semi-transparent
+]
+
+# prepare data for volume visualization
+nPoints = 256
+data_max = data.max()
+data = data / data_max  # normalize data
+np.clip(
+    data, a_min=-0.3, a_max=0.3, out=data
+)  # clip data between -30% and 30% of maximum
+data = (nPoints // 2 + nPoints * data).astype(
+    np.ubyte
+)  # convert data to byte-array with values ranging from 0 to 255
+
+# prepare voxel array
+cmap = pg.ColorMap(pos=np.linspace(-1, 1, len(colors)), color=colors)
+ctable = cmap.getLookupTable(start=-1, stop=1, nPts=nPoints, alpha=True, mode="byte")
+voxels = np.empty(data.shape + (4,), dtype=np.ubyte)
+voxels[..., 0] = ctable[data, 0]
+voxels[..., 1] = ctable[data, 1]
+voxels[..., 2] = ctable[data, 2]
+voxels[..., 3] = ctable[data, 3]
+
+# initialize GLViewWidget()
+app = QApplication([])
+w = gl.GLViewWidget()
+w.show()
+w.setWindowTitle("test of GLVolumeItem")
+w.setCameraPosition(distance=100)
+
+# display grid in (x,y)-plane
+g = gl.GLGridItem()
+g.setSize(x=12, y=12, z=12)
+g.scale(1 / dx, 1 / dy, 1 / dz)
+w.addItem(g)
+
+v = gl.GLVolumeItem(voxels, sliceDensity=1, smooth=True)
+v.translate(-nx / 2, -ny / 2, -nz / 2)
+w.addItem(v)
+
+app.exec()
---
+++ example/scripts/demo_PySide_Isosurface.py
@@ -0,0 +1,235 @@
+from pathlib import Path
+
+import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl  # pip install PyOpenGL
+from PySide6.QtWidgets import QApplication
+
+from kmap.library.misc import get_rotation_axes
+from kmap.library.orbital import Orbital
+
+
+class Plot3DMolecule:
+    def __init__(self, w, orbital, grid=True, photon={}, isoval=1 / 6):
+        data = orbital.psi["data"]
+        dx, dy, dz = orbital.psi["dx"], orbital.psi["dy"], orbital.psi["dz"]
+        nx, ny, nz = data.shape
+        bonds = orbital.get_bonds()
+
+        if grid:  # display grid in (x,y)-plane
+            g = gl.GLGridItem()
+            g.scale(1 / dx, 1 / dy, 1 / dz)
+            w.addItem(g)
+
+        items = []  # initialize list of plot items for later rotation
+
+        # add coordinate axes object
+        ax = gl.GLAxisItem()
+        ax.setSize(10, 10, 10)
+        items.append(ax)
+        w.addItem(ax)
+
+        # add bonds as LinePlotItems
+        for bond in bonds:
+            line = gl.GLLinePlotItem(
+                pos=bond, color=(0.8, 0.8, 0.8, 1), width=5, antialias=True
+            )
+            items.append(line)
+            w.addItem(line)
+
+        # compute isosurface for positive and negative isovalues
+        isovals = [+isoval * data.max(), -isoval * data.max()]
+        colors = [(1, 0.2, 0.2), (0.2, 0.2, 1)]  # red and blue
+
+        for iso, color in zip(isovals, colors):
+            # compute isosurface and center around origin
+            verts, faces = pg.isosurface(data, iso)
+            verts[:, 0] = verts[:, 0] - nx / 2
+            verts[:, 1] = verts[:, 1] - ny / 2
+            verts[:, 2] = verts[:, 2] - nz / 2
+
+            isosurface = gl.MeshData(vertexes=verts, faces=faces)
+            rgbt = np.zeros((isosurface.faceCount(), 4), dtype=float)
+            for c in range(3):
+                rgbt[:, c] = color[c]
+            rgbt[:, 3] = 1  # transparency (I guess)
+            isosurface.setFaceColors(rgbt)
+
+            p = gl.GLMeshItem(
+                meshdata=isosurface, smooth=True, shader="edgeHilight"
+            )  # shader options: 'balloon', 'shaded', 'normalColor', 'edgeHilight'
+            p.setGLOptions(
+                "translucent"
+            )  # choose between 'opaque', 'translucent' or 'additive'
+            w.addItem(p)
+
+            items.append(p)
+
+        if "polarization" in photon:  # display incident photon
+            alpha = photon["alpha"] * np.pi / 180
+            beta = (180 + photon["beta"]) * np.pi / 180
+            direction = [
+                np.sin(alpha) * np.cos(beta),
+                np.sin(alpha) * np.sin(beta),
+                np.cos(alpha),
+            ]
+            polarization = photon["polarization"]
+
+            ray_length = 50  # length of wavy light ray
+            amplitude = 5  # amplitude of oscillation
+            wavelength = 5  # wavelength of oscillation
+            n_points = 200  # number of points along wavy light ray
+
+            x0 = np.linspace(0, ray_length * direction[0], n_points)
+            y0 = np.linspace(0, ray_length * direction[1], n_points)
+            z0 = np.linspace(0, ray_length * direction[2], n_points)
+            t = np.linspace(0, ray_length, n_points)
+
+            if polarization == "s":
+                pol_1 = [np.sin(beta), -np.cos(beta), 0]
+                pol_2 = [0, 0, 0]
+
+            elif polarization == "p":
+                pol_1 = [0, 0, 0]
+                pol_2 = [
+                    np.cos(alpha) * np.cos(beta),
+                    np.cos(alpha) * np.sin(beta),
+                    -np.sin(alpha),
+                ]
+
+            elif polarization == "C+":
+                pol_1 = [np.sin(beta), -np.cos(beta), 0]
+                pol_2 = [
+                    np.cos(alpha) * np.cos(beta),
+                    np.cos(alpha) * np.sin(beta),
+                    -np.sin(alpha),
+                ]
+
+            elif polarization == "C-":
+                pol_1 = [np.sin(beta), -np.cos(beta), 0]
+                pol_2 = [
+                    -np.cos(alpha) * np.cos(beta),
+                    -np.cos(alpha) * np.sin(beta),
+                    +np.sin(alpha),
+                ]
+
+            elif (
+                polarization == "CDAD"
+            ):  # show C+ spiral ... until I have a better idea ...
+                pol_1 = [np.sin(beta), -np.cos(beta), 0]
+                pol_2 = [
+                    np.cos(alpha) * np.cos(beta),
+                    np.cos(alpha) * np.sin(beta),
+                    -np.sin(alpha),
+                ]
+
+            dx = amplitude * pol_1[0] * np.cos(
+                2 * np.pi * t / wavelength
+            ) + amplitude * pol_2[0] * np.sin(2 * np.pi * t / wavelength)
+            dy = amplitude * pol_1[1] * np.cos(
+                2 * np.pi * t / wavelength
+            ) + amplitude * pol_2[1] * np.sin(2 * np.pi * t / wavelength)
+            dz = amplitude * pol_1[2] * np.cos(
+                2 * np.pi * t / wavelength
+            ) + amplitude * pol_2[2] * np.sin(2 * np.pi * t / wavelength)
+
+            pos = np.array([x0 + dx, y0 + dy, z0 + dz]).T
+
+            photon_line = gl.GLLinePlotItem(
+                pos=pos, color=(1, 1, 0.0, 1), width=5, antialias=True
+            )
+            photon_line.setGLOptions("translucent")
+            w.addItem(photon_line)
+
+        self.items = items
+        self.phi = 0
+        self.theta = 0
+        self.psi = 0
+
+        return
+
+    def rotate_molecule(
+        self, phi=0, theta=0, psi=0
+    ):  # Rotate using Euler angles phi, theta, psi
+        old_phi = self.phi
+        old_theta = self.theta
+        old_psi = self.psi
+
+        # first rotate items back to original orientation
+        axes = get_rotation_axes(old_phi, old_theta)
+        for item in self.items:
+            item.rotate(
+                old_psi, axes[2][0], axes[2][1], axes[2][2], local=True
+            )  # first undo psi-rotation
+            item.rotate(
+                old_theta, axes[1][0], axes[1][1], axes[1][2], local=True
+            )  # next undo theta-rotation
+            item.rotate(
+                old_phi, axes[0][0], axes[0][1], axes[0][2], local=True
+            )  # finally undo phi-rotation
+
+        # now rotate items to new desired orientation
+        axes = get_rotation_axes(phi, theta)
+        for item in self.items:
+            item.rotate(
+                -phi, axes[0][0], axes[0][1], axes[0][2], local=True
+            )  # first do phi-rotation
+            item.rotate(
+                -theta, axes[1][0], axes[1][1], axes[1][2], local=True
+            )  # next do theta-rotation
+            item.rotate(
+                -psi, axes[2][0], axes[2][1], axes[2][2], local=True
+            )  # finally do psi-rotation
+
+        self.phi = phi
+        self.theta = theta
+        self.psi = psi
+
+        return
+
+
+### MAIN PROGRAM #######################################################
+
+# Path to data folder; replace with your own; use '/' instead of '+'
+# when concatenating with strings
+data_path = Path(__file__).parent / Path("../data/")
+
+cubefile = open(data_path / "PTCDA_B.cube").read()  # read cube-file from file
+molecule = Orbital(
+    cubefile
+)  # Orbital object contains molecular geometry and psi(x,y,z)
+
+# initialize GLViewWidget()
+app = QApplication([])
+w = gl.GLViewWidget()
+w.show()
+w.setWindowTitle("test")
+w.setCameraPosition(distance=100, elevation=90, azimuth=-90)  # view from top
+
+molecule_view = Plot3DMolecule(
+    w, molecule, photon={"polarization": "p", "alpha": 60, "beta": 115}
+)
+# molecule_view = Plot3DMolecule(w, molecule,
+#                photon={})
+
+
+# xdirection = gl.GLLinePlotItem(pos=np.array([[0,0,0],[20,0,0]]), color=(1,1,1,0.5), width=5, antialias=True)
+# w.addItem(xdirection)
+# ydirection = gl.GLLinePlotItem(pos=np.array([[0,0,0],[0,20,0]]), color=(1,0,0,0.5), width=5, antialias=True)
+# w.addItem(ydirection)
+
+
+# a few tests to see if the roration works as desired ...
+# (note: only last is shown, so uncomment all others)
+# molecule_view.rotate_molecule(phi=20,theta= 0,psi= 0)
+# molecule_view.rotate_molecule(phi= 0,theta= 0,psi= 0)
+# molecule_view.rotate_molecule(phi= 0,theta=40,psi= 0)
+# molecule_view.rotate_molecule(phi= 0,theta= 0,psi= 0)
+# molecule_view.rotate_molecule(phi=90,theta=20,psi= 0)
+# molecule_view.rotate_molecule(phi= 0,theta= 0,psi= 0)
+molecule_view.rotate_molecule(phi=0, theta=0, psi=0)
+# molecule_view.rotate_molecule(phi= 0,theta= 0,psi= 0)
+
+# ... all these tests were in agreement with the kmaps shown in the kMap GasPhaseSim-Tab!
+
+app.exec()
