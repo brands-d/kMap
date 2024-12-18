35ca772502f70ed56c40cd32c71178650fbdeab6
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2024-12-18 11:58:43 +0100

    Fixed tests

--- kmap/tests/test_application.py
+++ kmap/tests/test_application.py
@@ -1,12 +1,10 @@
 import unittest
-from PyQt5.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication
 from kmap import kMap
 
 
 class TestMainWindow(unittest.TestCase):
-
     app = kMap.kMap([])
 
     def test_default_state(self):
-
         self.app.test()
