e96afe93dc5797c498d37f7ef6d2c972735eda4a
Dominik Brandstetter <dominik.brandstetter@uni-graz.at>
2025-07-15 15:37:36 +0200

    Had to bump version because build files for pypi where wrong

--- kmap/__init__.py
+++ kmap/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 __project__ = "kMap.py"
-__version__ = "3.5.1"
+__version__ = "3.5.2"
 __date__ = "15.07.2025"
 __directory__ = Path(os.path.dirname(os.path.realpath(__file__)))
